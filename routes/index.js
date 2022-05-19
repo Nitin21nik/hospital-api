d366742de1d0516fd5ac8f6aed52cdc8135d2b59
Nitin21nik <96187297+Nitin21nik@users.noreply.github.com>
2022-05-19 18:38:11 +0530

    adding the index route

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ const router=express.Router();//requiring router
 
 console.log('router is loaded');
 
+router.get('/', function(req, res) {
+    return res.json(400, {
+        message: 'Welcome to Covid-19 ward.Please enter the correct routes from the documentation!!' 
+    }
+)});
+
 router.use('/api', require('./api'));//redirecting to api route according to the request route
 
 module.exports=router;
